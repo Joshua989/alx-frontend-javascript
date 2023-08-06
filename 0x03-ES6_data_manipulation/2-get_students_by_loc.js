1c7b28a78839e4c725190ee33bfd94e2d549581a
josipoli0222@gmail.com <josipoli0222@gmail.com>
2023-08-06 15:43:03 +0100

    use

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,3 @@
 export default function getStudentsByLocation(students, city) {
   return students.filter((arr) => arr.location === city);
 }
-//this code use the attay method filter under the condition of city function
